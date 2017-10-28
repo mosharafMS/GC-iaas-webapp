f7303aa38430b456bc5943369879ae13eed9c3d0
mosharafMS <Mohamed.Sharaf@Microsoft.com>
2017-10-28 18:50:34 -0400

    remove azureuserName and azurePassword from deploy.json-->configureSQL.json

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 
-az group deployment create --debug --template-file azuredeploy.json \ 
+az group deployment create --template-file azuredeploy.json \ 
 --parameters azuredeploy.parameters.json \ 
 --resource-group GCbluePrint  --verbose
