fa366688b5d11466ce0faf483e99615531a32aa0
mosharafMS <Mohamed.Sharaf@Microsoft.com>
2017-10-29 12:42:09 -0400

    adding comments for deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+#use with bash
+#make sure to execute ./predeply/Orchestration_InitialSetup.ps1 in a powershell session first
+
 
 az group deployment create --template-file azuredeploy.json \ 
 --parameters azuredeploy.parameters.json \ 
