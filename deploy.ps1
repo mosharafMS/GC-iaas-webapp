ed9667f36376ecf87294ec9842ad25ae94c91fff
Adiy Qasrawi <aqasrawi@microsoft.com>
2017-11-01 20:56:26 -0400

    Split up azure deploy to several files to split the deployment and reduce the likelihood of exceeding alloted time for a deployment.

--- deploy.ps1
+++ deploy.ps1
@@ -3,6 +3,37 @@
 #
 
 $timestamp = Get-Date -Format "yyyy-MM-dd_hh-mm-ss"
+#
+# Original deployment
+#
 New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName GCbluePrint `
 -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json `
+-Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
+
+#
+# First deployment - Storage Accounts
+#
+New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName GCbluePrint `
+-TemplateFile .\azuredeploy1.json -TemplateParameterFile .\azuredeploy.parameters.json `
+-Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
+
+#
+# Second deployment - Active Directory VMs
+#
+New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName GCbluePrint `
+-TemplateFile .\azuredeploy2.json -TemplateParameterFile .\azuredeploy.parameters.json `
+-Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
+
+#
+# Third deployment - Networking
+#
+New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName GCbluePrint `
+-TemplateFile .\azuredeploy3.json -TemplateParameterFile .\azuredeploy.parameters.json `
+-Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
+
+#
+# Fourth deployment - all the rest
+#
+New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName GCbluePrint `
+-TemplateFile .\azuredeploy4.json -TemplateParameterFile .\azuredeploy.parameters.json `
 -Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
