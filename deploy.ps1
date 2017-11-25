b29f56ec4031647490278432bb0f773e47057474
aqasrawi <aqasrawi@microsoft.com>
2017-11-25 13:00:23 +0000

    Removed dependency

--- deploy.ps1
+++ deploy.ps1
@@ -2,6 +2,11 @@
 # deploy.ps1
 #
 
+Login-AzureRmAccount
+
+$subscriptionId = '3a4af7b3-b7ac-463d-9940-1d80445961a8'
+ Set-AzureRmContext -SubscriptionId $subscriptionId
+
 $timestamp = Get-Date -Format "yyyy-MM-dd_hh-mm-ss"
 New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName GCbluePrint `
 -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json `
--- predeploy/Orchestration_InitialSetup.ps1
+++ predeploy/Orchestration_InitialSetup.ps1
@@ -1,5 +1,5 @@
 #requires -RunAsAdministrator
-#requires -Modules AzureRM
+
 
 
 
