c4e2cc65bee17fe9d324e2159c2f5b57ae3488c6
aqasrawi <aqasrawi@microsoft.com>
2018-01-21 20:29:57 -0500

    Changed API version again

--- deploy.ps1
+++ deploy.ps1
@@ -8,6 +8,6 @@ $subscriptionId = '3a4af7b3-b7ac-463d-9940-1d80445961a8'
  Set-AzureRmContext -SubscriptionId $subscriptionId
 
 $timestamp = Get-Date -Format "yyyy-MM-dd_hh-mm-ss"
-New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName GCbluePrint `
+New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName GCbluePrint1 `
 -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json `
 -Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
