9be9ceeebceee84086ac0034b1d8d46fd3845694
aqasrawi <aqasrawi@microsoft.com>
2018-01-23 11:42:38 -0500

    More backups de-coupling

--- deploy.ps1
+++ deploy.ps1
@@ -9,5 +9,5 @@ $subscriptionId = '3a4af7b3-b7ac-463d-9940-1d80445961a8'
 
 $timestamp = Get-Date -Format "yyyy-MM-dd_hh-mm-ss"
 New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName GCbluePrint `
--TemplateFile .\azuredeploy01.json -TemplateParameterFile .\azuredeploy.parameters.json `
+-TemplateFile .\azuredeploy02.json -TemplateParameterFile .\azuredeploy.parameters.json `
 -Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
