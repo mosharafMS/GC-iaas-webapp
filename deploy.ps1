62a48ef9c7575fbcffa6ebf5303ea061166d421f
aqasrawi <aqasrawi@microsoft.com>
2018-01-23 12:54:24 -0500

    More decoupling of Backups

--- deploy.ps1
+++ deploy.ps1
@@ -5,9 +5,29 @@
 #Login-AzureRmAccount
 
 $subscriptionId = '3a4af7b3-b7ac-463d-9940-1d80445961a8'
- Set-AzureRmContext -SubscriptionId $subscriptionId
-
+Set-AzureRmContext -SubscriptionId $subscriptionId
 $timestamp = Get-Date -Format "yyyy-MM-dd_hh-mm-ss"
-New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName GCbluePrint `
+$resourceGroupName = 'GCbluePrint'
+
+#
+# Provision Basic Services / OMS Workspaces
+#
+New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName $resourceGroupName `
+-TemplateFile .\azuredeploy01.json -TemplateParameterFile .\azuredeploy.parameters.json `
+-Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
+#
+# Provision Domain Controllers
+#
+New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName $resourceGroupName `
 -TemplateFile .\azuredeploy02.json -TemplateParameterFile .\azuredeploy.parameters.json `
+-Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
+
+Restart-AzureRmVM -ResourceGroupName "GCbluePrint" -Name "AZ-PDC-VMprod" -Verbose
+Restart-AzureRmVM -ResourceGroupName "GCbluePrint" -Name "AZ-BDC-VMprod" -Verbose
+
+#
+# Provision Domain Controllers Backups
+#
+New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName $resourceGroupName `
+-TemplateFile .\azuredeploy03.json -TemplateParameterFile .\azuredeploy.parameters.json `
 -Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
