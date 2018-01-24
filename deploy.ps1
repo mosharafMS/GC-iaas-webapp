6cf09be93e8248b4875da7ced29402d52cca8dec
aqasrawi <aqasrawi@microsoft.com>
2018-01-23 20:34:34 -0500

    change deploy to remove any secrets

--- deploy.ps1
+++ deploy.ps1
@@ -5,22 +5,24 @@
 
 #Login-AzureRmAccount
 
-$subscriptionId = '3a4af7b3-b7ac-463d-9940-1d80445961a8'
-Set-AzureRmContext -SubscriptionId $subscriptionId
+#$subscriptionId = 'xxxxxxxxxxxxxxxxxxxx'
+#Set-AzureRmContext -SubscriptionId $subscriptionId
 $timestamp = Get-Date -Format "yyyy-MM-dd_hh-mm-ss"
-$resourceGroupName = 'GCbluePrintUser1'
+$resourceGroupName = 'GCBlueprint'
+$keyvaultName="GCBlueprint1982"
+$parametersFile='.\azuredeploy.parameters.testing'
 
 #
 # Provision Basic Services / OMS Workspaces
 #
 New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName $resourceGroupName `
--TemplateFile .\azuredeploy01.json -TemplateParameterFile .\azuredeploy.parameters.json `
+-TemplateFile .\azuredeploy01.json -TemplateParameterFile $parametersFile `
 -Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
 #
 # Provision Domain Controllers
 #
 New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName $resourceGroupName `
--TemplateFile .\azuredeploy02.json -TemplateParameterFile .\azuredeploy.parameters.json `
+-TemplateFile .\azuredeploy02.json -TemplateParameterFile $parametersFile `
 -Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
 
 Restart-AzureRmVM -ResourceGroupName $resourceGroupName -Name "AZ-PDC-VMprod" -Verbose
@@ -32,14 +34,14 @@ Start-Sleep -Seconds 120
 # Provision Domain Controllers Backups
 #
 New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName $resourceGroupName `
--TemplateFile .\azuredeploy03.json -TemplateParameterFile .\azuredeploy.parameters.json `
+-TemplateFile .\azuredeploy03.json -TemplateParameterFile $parametersFile `
 -Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
 
 #
 # App GW, other VMs
 #
 New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName $resourceGroupName `
--TemplateFile .\azuredeploy04.json -TemplateParameterFile .\azuredeploy.parameters.json `
+-TemplateFile .\azuredeploy04.json -TemplateParameterFile $parametersFile `
 -Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
 
 #
@@ -53,10 +55,15 @@ New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName $reso
 # Encryption
 #
 
-$aadClientID = 'c96ad29f-b88f-4c64-ac5f-fb7897b6826d'
-$aadClientSecret = '89ee108b-570b-44b6-8c7f-315b11d670a1'
-$diskEncryptionKeyVaultUrl = 'https://gckeystoreUser1.vault.azure.net/'
-$keyVaultResourceId = '/subscriptions/3a4af7b3-b7ac-463d-9940-1d80445961a8/resourceGroups/GCbluePrintUser1/providers/Microsoft.KeyVault/vaults/GCKeystoreUser1'
+
+
+#$keyVaultResourceId = '/subscriptions/3a4af7b3-b7ac-463d-9940-1d80445961a8/resourceGroups/GCbluePrintUser1/providers/Microsoft.KeyVault/vaults/GCKeystoreUser1'
+
+$keyvault=Get-AzureRmKeyVault -VaultName $keyvaultName
+$aadClientID= (Get-AzureKeyVaultSecret -VaultName $keyvaultName -Name "aadClientID").SecretValutText
+$aadClientSecret=(Get-AzureKeyVaultSecret -VaultName $keyvaultName -Name "aadClientSecret").SecretValueText
+$keyVaultResourceId=$keyvault.ResourceId
+
 Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $resourceGroupName -VMName 'AZ-PDC-VMprod' -AadClientID $aadClientID -AadClientSecret $aadClientSecret -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl -DiskEncryptionKeyVaultId $keyVaultResourceId -VolumeType All -Verbose -Force
 Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $resourceGroupName -VMName 'AZ-BDC-VMprod' -AadClientID $aadClientID -AadClientSecret $aadClientSecret -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl -DiskEncryptionKeyVaultId $keyVaultResourceId -VolumeType All -Verbose -Force
 
